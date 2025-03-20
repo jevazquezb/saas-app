9e9caa58b8b4fd6b942f02dbc4b667d51ddf7e08
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-19 23:51:22 -0600

    Add a route with methods to create and delete posts

---
+++ app/api/post/route.js
@@ -0,0 +1,95 @@
+// An API endpoint to create a new Post document in the database. The route is not protected by the auth middleware, so anyone can create a new post. The route expects a POST request with a title and description in the request body. The boardId is in the query parameters. The userId is not required, as it is optional and will be set to the user's ID if they are logged in.
+
+import { NextResponse } from "next/server";
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/board";
+import Post from "@/models/post";
+import User from "@/models/user";
+import { auth } from "@/auth";
+import { Filter } from "bad-words";
+
+export async function POST(req) {
+  try {
+    const { title, description } = await req.json(); // body
+    const { searchParams } = req.nextUrl;
+    const boardId = searchParams.get("boardId");
+
+    const badWordsFilter = new Filter();
+    const sanitizedTitle = badWordsFilter.clean(title);
+    const sanitizedDescription = badWordsFilter.clean(description);
+
+    if (!sanitizedTitle) {
+      return NextResponse.json(
+        { error: "Post title is required." },
+        { status: 400 }
+      );
+    }
+
+    const session = await auth();
+
+    await connectMongo();
+
+    const board = await Board.findById(boardId);
+
+    if (!board) {
+      return NextResponse.json({ error: "Board not found." }, { status: 404 });
+    }
+
+    const post = await Post.create({
+      title: sanitizedTitle,
+      description: sanitizedDescription,
+      boardId,
+      userId: session?.user?.id,
+    });
+
+    return NextResponse.json(post);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { searchParams } = req.nextUrl;
+    const postId = searchParams.get("postId");
+
+    if (!postId) {
+      return NextResponse.json(
+        { error: "postId is required." },
+        { status: 400 }
+      );
+    }
+
+    const session = await auth();
+
+    await connectMongo();
+
+    const user = await User.findById(session?.user?.id);
+
+    if (!user.hasAccess) {
+      return NextResponse.json(
+        { error: "Please subscribe first." },
+        { status: 403 }
+      );
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found." }, { status: 404 });
+    }
+
+    if (!user.boards.includes(post.boardId.toString())) {
+      return NextResponse.json(
+        { error: "Not authorized! You can only delete your own posts." },
+        { status: 401 }
+      );
+    }
+
+    await Post.deleteOne({ _id: postId });
+
+    return NextResponse.json({ message: "Post deleted." });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
