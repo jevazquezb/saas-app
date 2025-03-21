fce2e761599572cae074a659a75a73dc64de2064
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-21 08:21:20 -0600

    Add the ButtonVote to the CardPost component

--- components/card-post.js
+++ components/card-post.js
@@ -1,15 +1,17 @@
+import ButtonVote from "./button-vote";
+
 const CardPost = ({ post }) => {
   return (
-    <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-center">
+    <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-start gap-3">
       <div>
-        <div className="font-bold mb-1">{post.title}</div>
+        <div className="font-bold mb-1 text-lg">{post.title}</div>
         {post.description && (
           <p className="opacity-80 leading-relaxed max-h-32 overflow-y-auto">
             {post.description}
           </p>
         )}
       </div>
-      <button className="btn btn-square">🔼</button>
+      <ButtonVote postId={`${post._id}`} initialVotes={post.votesCounter} />
     </li>
   );
 };
