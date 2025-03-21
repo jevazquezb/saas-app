28e77565616f9340ab15eb49729f9b47a017422d
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-21 10:34:50 -0600

    Listen for posts order and rerender them in the DOM

--- app/board/[id]/page.js
+++ app/board/[id]/page.js
@@ -1,38 +1,50 @@
-import connectMongo from "@/libs/mongoose";
-import Board from "@/models/board";
-import Post from "@/models/post";
-import { redirect } from "next/navigation";
+"use client";
+
+import { useState, useEffect } from "react";
+import { getData } from "@/utilities/helpers";
 import FormAddPost from "@/components/form-add-post";
 import CardPost from "@/components/card-post";
 
-const getData = async (boardId) => {
-  await connectMongo();
-
-  const board = await Board.findById(boardId);
-  const posts = await Post.find({ boardId }).sort({ votesCounter: -1 }); // 1 or -1. -1 orders by the most recent post
-
-  if (!board) {
-    redirect("/");
-  }
-
-  return { board, posts };
-};
-
-export default async function PublicFeedbackBoard({ params }) {
+export default function PublicFeedbackBoard({ params }) {
   const { id } = params;
-  const { board, posts } = await getData(id);
+  const [board, setBoard] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { board, posts } = await getData(id);
+      setBoard(board);
+      setPosts(posts);
+    };
+
+    fetchData();
+  }, [id]);
+
+  const handleVoteChange = async () => {
+    const { posts } = await getData(id);
+    setPosts(posts);
+  };
+
+  const handlePostAdded = async () => {
+    const { posts } = await getData(id);
+    setPosts(posts);
+  };
 
   return (
     <main className="min-h-screen bg-base-200">
       <section className="max-w-5xl mx-auto p-5">
-        <h1 className="text-lg font-bold">{board.name} (public)</h1>
+        <h1 className="text-lg font-bold">{board?.name} (public)</h1>
       </section>
 
       <section className="max-w-5xl mx-auto px-5 flex flex-col md:flex-row items-start gap-8 pb-12">
-        <FormAddPost boardId={id} />
+        <FormAddPost boardId={id} onPostAdded={handlePostAdded} />
         <ul className="space-y-4 flex-grow">
           {posts.map((post) => (
-            <CardPost key={`${post._id}`} post={post} />
+            <CardPost
+              key={post._id}
+              post={post}
+              onVoteChange={handleVoteChange}
+            />
           ))}
         </ul>
       </section>
