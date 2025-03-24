31207c090686d89a657deecf09ef5287d475652d
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-24 12:36:01 -0600

    Remove theme from layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <div>
           <Toaster />
