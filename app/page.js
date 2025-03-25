74b95a494d3e7ea8e4bc2bc96a0406a477267295
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-24 22:20:49 -0600

    Remove console.log()

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
-  console.log(session);
 
   const priceBenefits = [
     "Collect customer feedback",
