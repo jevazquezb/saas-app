6ebc9e9279deaebef8ee3a291c3691d7d0c13214
jesus-vazquez <je.vazquezb@gmail.com>
2025-03-25 14:17:45 -0600

    Revert "Add redirect callback"
    This reverts commit 32ae51ec280ae1cde5951cb0e00104a19b2a50ff.

--- auth.js
+++ auth.js
@@ -16,27 +16,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       // authorization: {
-      //   url: "https://accounts.google.com/o/oauth2/auth/authorize?response_type=code&prompt=login",
-      //   params: {
-      //     prompt: "consent",
-      //     access_type: "offline",
-      //     response_type: "code",
-      //   },
+      // url: "https://accounts.google.com/o/oauth2/auth/authorize?response_type=code&prompt=login",
+      // params: {
+      //   prompt: "consent",
+      //   access_type: "offline",
+      //   response_type: "code",
+      // },
       // },
       allowDangerousEmailAccountLinking: true,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // Redirect on login displays query parameters in the live site's URL.
-      // Ensure that the user is always redirected to the dashboard after login w/o the query parameters.
-      if (url.startsWith(baseUrl)) {
-        return `${baseUrl}/dashboard`;
-      } else if (url.startsWith("/")) {
-        return `${baseUrl}${url}`;
-      }
-      return baseUrl;
-    },
-  },
 });
